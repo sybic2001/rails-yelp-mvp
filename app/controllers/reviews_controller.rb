33cca9c29a75f20b2af97f95777f615e9c0af572
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-05-11 17:30:59 +0200

    avec etoile qui s'allument

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,4 @@
 class ReviewsController < ApplicationController
-  def new
-    @review = Review.new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-  end
 
   def create
     @review = Review.new(review_params)
@@ -11,7 +7,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render "restaurants/show"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :restaurants, only: [:new, :index, :create, :show] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
